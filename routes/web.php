8330f511d8bc4551ec477e1c8d954eedaeb20bd9
ilhamkibo <ilham.ilam59@gmail.com>
2022-11-24 14:44:40 +0700

    feat:integrate sidebar

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\User\dashboardController;
 use App\Http\Controllers\User\MovieController;
+use App\Http\Controllers\User\SubscriptionPlanController;
 use Illuminate\Foundation\Application;
 use Illuminate\Routing\RouteGroup;
 use Illuminate\Support\Facades\Route;
@@ -23,6 +24,8 @@ Route::redirect('/','/login');
 Route::middleware(['auth','role:user'])->prefix('dashboard')->name('user.dashboard.')->group(function() {
     Route::get('/',[dashboardController::class, 'index'])->name('index');
     Route::get('movie/{movie:slug}', [MovieController::class, 'show'])->name('movie.show');
+    Route::get('subscription-plan', [SubscriptionPlanController::class, 'index'])->name('subscriptionPlan.index');
+    Route::post('subscription-plan/{subscriptionPlan}/user-subscribe', [SubscriptionPlanController::class, 'userSubscribe'])->name('subscriptionPlan.userSubscribe');
 });
 
 Route::prefix('prototype')->name('prototype.')->group(function() {
@@ -44,7 +47,7 @@ Route::prefix('prototype')->name('prototype.')->group(function() {
 
     Route::get('/movie/{slug', function() {
         return Inertia::render('Prototype/Movie/Show');
-    })->name('movie.show');
+    })->name('movie.show'); 
 });
 
 require __DIR__.'/auth.php';
